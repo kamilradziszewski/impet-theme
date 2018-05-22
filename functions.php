94ec18310754d094b6340eee1bdc45a2d5b4f277
Kamil Radziszewski <contact@kamilradziszewski.com>
2018-05-22 18:59:11 +0200

    Add TGM Plugin Activation

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@ if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
 	});
-	
+
 	add_filter('template_include', function($template) {
 		return get_stylesheet_directory() . '/static/no-timber.html';
 	});
-	
+
 	return;
 }
 
@@ -60,3 +60,63 @@ class StarterSite extends TimberSite {
 }
 
 new StarterSite();
+
+
+
+
+
+/*******************************************************************************
+ * Include the TGM_Plugin_Activation class.
+ */
+require_once get_template_directory() . '/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'impet_bialystok_register_required_plugins' );
+
+/**
+ * Register the required plugins for this theme.
+ */
+function impet_bialystok_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+    // Required plugins
+    array(
+			'name'               => 'Timber',
+			'slug'               => 'timber-library',
+			'required'           => true,
+			'force_activation'   => true,
+    ),
+
+    array(
+			'name'               => 'CMB2',
+			'slug'               => 'cmb2',
+			'required'           => true,
+			'force_activation'   => true,
+    ),
+
+    // Recommended plugins
+    array(
+			'name'               => 'All-in-One WP Migration',
+			'slug'               => 'all-in-one-wp-migration',
+    ),
+
+	);
+
+	$config = array(
+		'id'           => 'impet-bialystok',       // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
